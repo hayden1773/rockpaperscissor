4ad7b905c78602706670133de120a1d9cc6c6e42
Hayden Kiltoff <hayden060@gmail.com>
2022-03-30 11:47:59 -0700

    added java code for game

---
+++ script.js
@@ -0,0 +1,59 @@
+
+// //TODO: user chooses r,p, s
+// // var user = [" rock ", " paper ", " scissors "]
+// // var comp = [" rock ", " paper ", " scissors "]
+// var options = (" rock " + " paper " + " scissors ")
+// var userPoints = 0
+// var compPoints = 0
+
+// function random_items() {
+//     return comp[(math.floor(Math.random) * comp.length)]
+// }
+// //TODO: ask user via pop up what choice they want
+// var choice = prompt("please choose " + user)
+// //TODO: save to a variable so we can reference later
+// console.log(random_items())
+
+// //TODO: EGDE CASE: user doenst pick r,p, or s
+
+// //if value doesnt match
+// //alert user to try again
+// prompt("try again")
+// //computer generate its decicion
+// //TODO: EGDE CASE: user leaves field blank 
+
+// //tie win lose
+// function game(){
+//     var user = prompt("choose between rock, paper, and scissors")
+//     alert("user choice is " + user) 
+//     var comp= random_items()
+//     alert ("computer choice is " + comp)
+// if (user == comp) {
+    
+//   prompt("draw");  //tie
+// } else if ((user === "rock" && comp === "scissors") || (user === "paper" && comp === "rock") || (user === "scissors" && comp === "paper"))
+
+
+
+// }
+//     //user p > comp p == draw
+//     //user p > comp s == comp
+//     //user p > comp r == user
+//     //user s > comp s == draw
+//     //user s > comp p == user 
+//     //user s > comp r == comp
+//     //user r > comp r == draw
+//     //user r > comp s == user
+//     //user r > comp p == comp
+
+//     //TODO: computer chooses r,p,s
+//     // var comp=[" rock "," paper "," scissors "]
+//     //TODO: determine winner, show to user
+//     alert("show stats")
+// // function comp(){
+// //     return random_items(math.floor(Math.random) * comp.length)
+
+// // }
+// //TODO: display stats
+// //TODO: ask to play again
+// prompt("play again?")
